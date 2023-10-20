1df9c0aec099029f8df9473c8b1510668802004a
wlzhou <wlzhou@grandstream.cn>
2023-10-20 17:16:00 +0800

    remote conf add timer

--- app/src/main/java/com/gs/panel/viewmodel/RemoteConfViewModel.kt
+++ app/src/main/java/com/gs/panel/viewmodel/RemoteConfViewModel.kt
@@ -4,6 +4,12 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import com.gs.panel.state.LocalConfState
+import com.gs.panel.util.TimeUtil
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 enum class RemoteConfState {
     IDLE,
@@ -14,7 +20,7 @@ enum class RemoteConfState {
 }
 
 class RemoteConfViewModel : ViewModel() {
-    var confState by mutableStateOf(RemoteConfState.RUN)
+    var confState by mutableStateOf(RemoteConfState.IDLE)
     var showErrorDialog by mutableStateOf(false)
     var showMoreDeviceDialog by mutableStateOf(false)
     var showDelayConfDialog by mutableStateOf(false)
@@ -22,4 +28,26 @@ class RemoteConfViewModel : ViewModel() {
     var showStopConfDialog by mutableStateOf(false)
     var showStartConfSuccessDialog by mutableStateOf(false)
     var showDelayConfSuccessDialog by mutableStateOf(false)
+
+    private val mainScope = MainScope()
+    private var timeJob: Job
+
+    private var index = 0
+    private val confStateList = listOf<RemoteConfState>(
+        RemoteConfState.IDLE,
+        RemoteConfState.READY_FLAG,
+        RemoteConfState.READY,
+        RemoteConfState.RUN,
+        RemoteConfState.DISABLE,
+    )
+
+    init {
+        timeJob = mainScope.launch {
+            repeat(Int.MAX_VALUE) {
+                delay(10000)
+                index = (index + 1) % 5
+                confState = confStateList[index]
+            }
+        }
+    }
 }
