f3ebc80cdb8dab002eb3434511c31ee1381c7ce3
wlzhou <wlzhou@grandstream.cn>
2023-11-01 11:39:03 +0800

    commit

---
+++ app/src/main/java/com/gs/panel/screen/RemoteConfIdleScreen.kt
@@ -0,0 +1,301 @@
+package com.gs.panel.screen
+
+import android.util.Log
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.FlowRow
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavController
+import com.gs.panel.R
+import com.gs.panel.entity.deviceList
+import com.gs.panel.entity.deviceMoreList
+import com.gs.panel.state.DialogState
+import com.gs.panel.ui.theme.CustomColor
+import com.gs.panel.viewmodel.RemoteConfState
+//import com.gs.panel.viewmodel.ConfState
+import com.gs.panel.viewmodel.RemoteConfViewModel
+import com.gs.panel.widget.ClickButtonWidget
+import com.gs.panel.widget.DelayConfDialog
+import com.gs.panel.widget.DelayConfSuccessDialog
+import com.gs.panel.widget.DynamicsRowWidget
+import com.gs.panel.widget.ErrorDialog
+import com.gs.panel.widget.ErrorTipWidget
+import com.gs.panel.widget.MoreDeviceDialog
+import com.gs.panel.widget.StartConfDialog
+import com.gs.panel.widget.StartConfSuccessDialog
+import com.gs.panel.widget.StopConfDialog
+
+@OptIn(ExperimentalLayoutApi::class)
+@Composable
+fun RemoteConfIdleScreen(
+    navController: NavController,
+    viewModel: RemoteConfViewModel
+) {
+    Box(modifier = Modifier
+        .fillMaxSize()
+        .background(Color(0xFF00a645))
+
+//        .border(1.dp, Color.Black)
+    ) {
+        Box(modifier = Modifier
+            .fillMaxSize()
+//            .padding(vertical = 20.dp)
+            .padding(0.dp, 70.dp, 0.dp, 20.dp)
+//            .background(CustomColor.blush)
+        ) {
+            Column(modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 30.dp)
+                .background(CustomColor.cranesbill)
+                .align(Alignment.TopCenter)) {
+                if (viewModel.facilityList.isNotEmpty()) {
+                    DynamicsRowWidget(modifier = Modifier
+//                        .background(CustomColor.blue)
+                        .fillMaxWidth()
+                        .height(34.dp)) {
+                        viewModel.facilityList.forEachIndexed { index, facilityItem ->
+                            if (facilityItem.confFacilityId == 0) {
+                                Text(
+                                    text = "More",
+                                    modifier = Modifier
+                                        .background(Color.White)
+                                        .border(1.dp, Color(0xFF8eb68e))
+                                        .clickable { viewModel.openMoreDeviceDialog() }
+                                        .padding(6.dp, 3.dp, 6.dp, 3.dp),
+                                    color = Color(0xFF00a645),
+                                    fontSize = 20.sp,)
+                            } else {
+                                Text(
+                                    text = facilityItem.confFacilityName,
+                                    modifier = Modifier
+                                        .background(Color(0xFF30831f))
+                                        .border(1.dp, Color(0xFF8eb68e))
+                                        .padding(6.dp, 3.dp, 6.dp, 3.dp),
+                                    color = Color(0xFFecece1),
+                                    fontSize = 20.sp,)
+                            }
+                        }
+                    }
+                } else {
+                    Box(modifier = Modifier
+//                        .background(CustomColor.blue)
+                        .fillMaxWidth()
+                        .height(34.dp))
+                }
+                Spacer(modifier = Modifier
+                    .height(30.dp)
+                    .fillMaxWidth()
+//                    .background(CustomColor.addicted)
+                )
+                Text(
+                    text = viewModel.confName,
+                    modifier = Modifier
+//                        .background(CustomColor.green)
+                        .fillMaxWidth(),
+                    fontSize = 46.sp,
+                    color = Color.White,
+                    textAlign = TextAlign.Center
+                )
+            }
+            Column(modifier = Modifier
+                .fillMaxWidth()
+                .background(CustomColor.tree)
+                .align(Alignment.Center)) {
+                Text(
+                    text = "空闲",
+                    modifier = Modifier
+                        .background(CustomColor.blue)
+                        .fillMaxWidth(),
+                    color = Color.White,
+                    fontSize = 88.sp,
+                    textAlign = TextAlign.Center
+                )
+                Spacer(modifier = Modifier
+                    .height(60.dp)
+                    .fillMaxWidth()
+                    .background(CustomColor.powder))
+                ClickButtonWidget(
+                    modifier = Modifier
+                        .width(280.dp)
+                        .height(88.dp)
+                        .align(Alignment.CenterHorizontally),
+                    name = "立即开会",
+                    backgroundColor = Color.White,
+                    textColor = Color(0xFF00a645),
+                    onClick = {  }
+                )
+            }
+            Column(modifier = Modifier
+                .fillMaxWidth()
+                .background(CustomColor.fizz)
+                .align(Alignment.BottomCenter)) {
+                Row(modifier = Modifier
+                    .fillMaxWidth()
+                    .background(CustomColor.sand)
+                    .padding(horizontal = 30.dp)
+                ) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.btn_clock),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .size(26.dp)
+                            .align(Alignment.CenterVertically),
+                        tint = Color.White
+                    )
+                    Spacer(modifier = Modifier.width(20.dp))
+                    Text(text = "下一场", fontSize = 26.sp, color = Color.White)
+                    Spacer(modifier = Modifier.width(20.dp))
+                    Text(text = "10:30-11:30", fontSize = 26.sp, color = Color.White)
+                    Spacer(modifier = Modifier.width(20.dp))
+                    Text(text = "Alice(3702)", fontSize = 26.sp, color = Color.White)
+                    Row(
+                        modifier = Modifier
+                            .weight(1f)
+//                            .background(CustomColor.cranesbill)
+                            .align(Alignment.CenterVertically),
+                        horizontalArrangement = Arrangement.End,
+                    ) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.btn_list),
+                            contentDescription = null,
+                            modifier = Modifier
+                                .size(34.dp)
+                                .align(Alignment.CenterVertically)
+                                .background(CustomColor.fizz)
+                                .clickable { navController.navigate("confList") },
+                            tint = Color.White
+                        )
+                        Spacer(modifier = Modifier.width(44.dp))
+                        Icon(
+                            painter = painterResource(id = R.drawable.btn_setting),
+                            contentDescription = null,
+                            modifier = Modifier
+                                .size(34.dp)
+                                .align(Alignment.CenterVertically)
+                                .background(CustomColor.gall)
+                                .clickable {
+                                    viewModel.showErrorDialog = !viewModel.showErrorDialog
+                                },
+                            tint = Color.White
+                        )
+                    }
+                }
+                Row(modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(30.dp, 8.dp, 30.dp, 8.dp)
+                    .background(CustomColor.tree)
+                ) {
+                    Text(
+                        text = "会议主题会议主题会议会议主题会议主题会议主题会议主题会议主题会议主题…",
+                        fontSize = 26.sp,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .background(CustomColor.powder),
+                        color = Color.White)
+                }
+                Row(modifier = Modifier
+                    .fillMaxWidth()
+                    .height(60.dp)
+                    .background(CustomColor.tree)) {
+                    Spacer(modifier = Modifier
+                        .weight(1f)
+                        .fillMaxHeight())
+                    Box(modifier = Modifier
+                        .weight(48f)
+                        .fillMaxHeight()) {
+                        Row(modifier = Modifier.fillMaxSize()) {
+                            for (i in 1..24) {
+                                Box(modifier = Modifier
+                                    .weight(1f)
+                                    .fillMaxHeight()
+                                    .background(CustomColor.addicted)
+                                    .border(1.dp, Color.Gray))
+                            }
+                        }
+                        Row(modifier = Modifier.fillMaxSize()) {
+                            for (i in 1..24) {
+                                if (i >= 12) {
+                                    Box(modifier = Modifier
+                                        .weight(1f)
+                                        .fillMaxHeight()
+                                        .background(CustomColor.cranesbill)
+                                        .border(1.dp, Color.Gray))
+                                } else {
+                                    Box(modifier = Modifier
+                                        .weight(1f)
+                                        .fillMaxHeight()
+                                        .background(CustomColor.addicted)
+                                        .border(1.dp, Color.Gray))
+                                }
+                            }
+                        }
+                        Row(modifier = Modifier.fillMaxSize()) {
+                            for (i in 1..24) {
+                                if (i <= 6) {
+                                    Box(modifier = Modifier
+                                        .weight(1f)
+                                        .fillMaxHeight()
+                                        .background(CustomColor.green)
+                                        .border(1.dp, Color.Gray))
+                                } else {
+                                    Box(modifier = Modifier
+                                        .weight(1f)
+                                        .fillMaxHeight()
+                                        .background(Color.Transparent)
+                                        .border(1.dp, Color.Gray))
+                                }
+                            }
+                        }
+                    }
+                    Spacer(modifier = Modifier
+                        .weight(1f)
+                        .fillMaxHeight())
+                }
+                Row(modifier = Modifier
+                    .fillMaxWidth()
+                    .background(CustomColor.green)) {
+                    for (i in 0..24) {
+                        Text(text = "$i",
+                            modifier = Modifier
+                                .weight(1f)
+//                                .background(CustomColor.powder)
+//                                .border(1.dp, Color.Gray)
+                            ,
+                            textAlign = TextAlign.Center,
+                            color = Color.White
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
