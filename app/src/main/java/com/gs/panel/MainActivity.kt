3b9c60c29499fffbf17e3f9ce051d5cb31e7a376
wlzhou <wlzhou@grandstream.cn>
2023-11-15 17:12:25 +0800

    commit

--- app/src/main/java/com/gs/panel/MainActivity.kt
+++ app/src/main/java/com/gs/panel/MainActivity.kt
@@ -3,13 +3,7 @@ package com.gs.panel
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -17,15 +11,12 @@ import com.gs.panel.screen.ConfListScreen
 import com.gs.panel.screen.LocalConfScreen
 import com.gs.panel.screen.MainScreen
 import com.gs.panel.screen.RemoteConfScreen
-import com.gs.panel.ui.theme.CustomColor
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Box(modifier = Modifier.width(1024.dp).height(600.dp).border(1.dp, CustomColor.powder)) {
-                NavigationScreen()
-            }
+            NavigationScreen()
         }
     }
 }
