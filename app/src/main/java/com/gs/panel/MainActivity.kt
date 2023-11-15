a2ff0be70dd85e1570dee05f85712210f4db24c2
wlzhou <wlzhou@grandstream.cn>
2023-11-15 17:03:38 +0800

    adapter 1024 * 600 screen

--- app/src/main/java/com/gs/panel/MainActivity.kt
+++ app/src/main/java/com/gs/panel/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.gs.panel
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -12,21 +17,16 @@ import com.gs.panel.screen.ConfListScreen
 import com.gs.panel.screen.LocalConfScreen
 import com.gs.panel.screen.MainScreen
 import com.gs.panel.screen.RemoteConfScreen
-import com.gs.panel.util.TimeUtil
-import com.gs.panel.util.getNavigationBarHeight
-import com.gs.panel.util.getScreenHeight
-import com.gs.panel.util.getScreenWidth
-import com.gs.panel.util.getStatusBarHeight
+import com.gs.panel.ui.theme.CustomColor
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            NavigationScreen()
+            Box(modifier = Modifier.width(1024.dp).height(600.dp).border(1.dp, CustomColor.powder)) {
+                NavigationScreen()
+            }
         }
-        Log.d("wlzhou", "w = ${getScreenWidth()}, h = ${getScreenHeight()}")
-        Log.d("wlzhou", "date = ${TimeUtil.getTodayDate()}")
-        Log.d("wlzhou", "statusBar = ${getStatusBarHeight()}, navigationBar = ${getNavigationBarHeight()}")
     }
 }
 
