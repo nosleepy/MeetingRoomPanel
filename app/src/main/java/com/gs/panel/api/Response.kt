da011799c5e26ea1bd6249da94fe5fca18282e20
wlzhou <wlzhou@grandstream.cn>
2023-11-21 12:02:43 +0800

    add error code

--- app/src/main/java/com/gs/panel/api/Response.kt
+++ app/src/main/java/com/gs/panel/api/Response.kt
@@ -17,6 +17,8 @@ data class Response<T>(
             CODE_UCM_LOGIN_FAIL -> "错误的用户名或密码"
             CODE_MULTIPLE_LOGIN -> "登录次数过多"
             CODE_TIME_OCCUPY -> "预约时间已经被占用"
+            CODE_CONF_TIME_LIMIT -> "会议时长到达上限"
+            CODE_ROOM_DISABLE -> "会议室被禁用，预约会议失败"
             CODE_NO_NETWORK -> "网络异常，请检查"
             CODE_CONNECT_FAIL -> "服务器连接失败，请联系管理员"
             CODE_OTHER_EXCEPTION -> "其他异常"
@@ -31,6 +33,8 @@ data class Response<T>(
         const val CODE_UCM_LOGIN_FAIL = -37
         const val CODE_MULTIPLE_LOGIN = -68
         const val CODE_TIME_OCCUPY = -127
+        const val CODE_CONF_TIME_LIMIT = -198
+        const val CODE_ROOM_DISABLE = -231
         const val CODE_NO_NETWORK = -500
         const val CODE_CONNECT_FAIL = -501
         const val CODE_OTHER_EXCEPTION = -1000
