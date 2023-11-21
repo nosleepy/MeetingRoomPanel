0fac0477a9e42c88f733ee888ac00cf73dbcb3b2
wlzhou <wlzhou@grandstream.cn>
2023-11-21 14:24:41 +0800

    add alarm.png

--- app/src/main/java/com/gs/panel/widget/ErrorTip.kt
+++ app/src/main/java/com/gs/panel/widget/ErrorTip.kt
@@ -3,16 +3,22 @@ package com.gs.panel.widget
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.gs.panel.R
 
 @Composable
 fun ErrorTip(content: String) {
@@ -25,6 +31,13 @@ fun ErrorTip(content: String) {
             .clickable {},
         verticalAlignment = Alignment.CenterVertically
     ) {
+        Icon(
+            painter = painterResource(id = R.drawable.alarm),
+            contentDescription = null,
+            modifier = Modifier.size(26.dp),
+            tint = Color(0xFFe40426)
+        )
+        Spacer(modifier = Modifier.width(12.dp))
         Text(text = content, color = Color(0xFFe40426), fontSize = 22.sp)
     }
 }
